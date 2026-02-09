397741dedda9fb3db4a43822b622e9ea59c61984
kikukafandi <jtierta@gmail.com>
2026-02-09 15:22:25 +0700

    fix: register users

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { ValidationService } from './validation/validation.service';
 
+@Global()
 @Module({
-  providers: [PrismaService, ValidationService]
+  providers: [PrismaService, ValidationService],
+  exports: [PrismaService, ValidationService],
 })
 export class CommonModule {}
